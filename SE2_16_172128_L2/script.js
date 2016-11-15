4f929bb6729676dcb63999b56c938362f7f3f578
meisbest <erry.meis@gmail.com>
2016-11-15 15:47:13 +0100

    If the item already exists updates the quantity, the closes the form

--- SE2_16_172128_L2/script.js
+++ SE2_16_172128_L2/script.js
@@ -1,26 +1,43 @@
 /*Maximum number of items in the warehouse*/
 var maxi = 30;
 
+/* Array of the actual objects */
+var objs = ["bulloni", "chiodi"];
+
 /* Shows the form for the insertion of new items and autofocuses on the first field */
 function showModulo() {
 	document.getElementById("modulo").style.display = "block";
 	document.getElementById("nome").focus();
 }
 
-/* Adds a row in the table based on the user's input */
+/* Adds a row in the table based on the user's input, or updates the quantity. Then resets and closes the form. */
 function addCose() {
 	var nome = document.getElementById("nome").value.toLowerCase();
 	var qta = document.getElementById("qta").valueAsNumber;
-	var row = document.createElement("DIV");
-	var col1 = document.createElement("DIV");
-	var col2 = document.createElement("DIV");
-	var text1 = document.createTextNode(nome);
-	var text2 = document.createTextNode(qta);
-	col1.appendChild(text1);
-	col2.appendChild(text2);
-	row.appendChild(col1);
-	row.appendChild(col2);
-	document.getElementById("mainTable").appendChild(row);
+	var index = objs.indexOf(nome);
+	if(index < 0) {
+		var row = document.createElement("DIV");
+		var col1 = document.createElement("DIV");
+		var col2 = document.createElement("DIV");
+		var text1 = document.createTextNode(nome);
+		var text2 = document.createTextNode(qta);
+		col1.appendChild(text1);
+		col2.appendChild(text2);
+		row.appendChild(col1);
+		row.appendChild(col2);
+		document.getElementById("mainTable").appendChild(row);
+		objs.push(nome);
+	}
+	else {
+		var table = document.getElementById("mainTable");
+		var qta_old = table.getElementsByTagName("div")[index*3+5];
+		var qta_new = parseInt(qta_old.innerHTML) + qta;
+		qta_old.innerHTML = qta_new;
+	}
+	
+	document.getElementById("modulo").style.display = "none";
+	document.getElementById("nome").value = "";
+	document.getElementById("qta").value = "";
 }
 
 /* Regulates the max number of items */
