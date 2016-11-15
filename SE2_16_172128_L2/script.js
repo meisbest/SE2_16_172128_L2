76dc8b96dd7625ccb3af26631b5bbcd9c503768c
meisbest <erry.meis@gmail.com>
2016-11-15 15:31:12 +0100

    Added autofocus for popup form and created the max-item rule

--- SE2_16_172128_L2/script.js
+++ SE2_16_172128_L2/script.js
@@ -1,3 +1,12 @@
+var maxi = 30;
+
 function showModulo() {
 	document.getElementById("modulo").style.display = "block";
+	document.getElementById("nome").focus();
+}
+
+function modMax() {
+	var newmax = document.getElementById("new_max").value;
+	document.getElementById("old_max").textContent = newmax;
+	maxi = newmax;
 }
